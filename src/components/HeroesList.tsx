9e53da1f7b393f2929656c11d1af9d1b6f9344a6
salaz103 <salaz103@gmail.com>
2022-02-27 00:43:51 -0600

    Implementing react-window

--- src/components/HeroesList.tsx
+++ src/components/HeroesList.tsx
@@ -1,39 +1,62 @@
 import React, { useState } from 'react';
 import { connect, useSelector } from 'react-redux';
 import { HeroesState } from '../reducers/heroesReducer';
+import { FixedSizeGrid as Grid, GridChildComponentProps, ListChildComponentProps } from 'react-window';
 import getVisibleHeroes from '../selector/heroes';
 import HeroeListItem from './HeroeListItem';
+import '../styles.css'
+
 
 
 function HeroesList() {
-    const [searchTerm,setSearchTerm]= useState("");
+    const [searchTerm, setSearchTerm] = useState("");
     const heroes = useSelector<HeroesState, HeroesState["heroes"]>((state) => state.heroes)
-    //const finalHeroes= getVisibleHeroes(heroes,filter);
+    console.log(heroes);
+
+    const Cell = ({ columnIndex, rowIndex, style }: GridChildComponentProps) => (
+        <div style={style}>
+            {
+                heroes.filter((val) => {
+                    if (searchTerm == "") {
+                        return val
+                    } else if (val.name.toLocaleLowerCase().includes(searchTerm.toLowerCase())) {
+                        return val
+                    }
+                }).map((heroe) => {
+                    return <HeroeListItem key={heroe.id} {...heroe} />
+                })
+            }
+        </div>
+    );
 
     return (
         <div>
             <input
-            type="text"
-            placeholder='Look for more Heroes'
-            onChange={(event)=>{
-                setSearchTerm(event.target.value)
-            }}
+                type="text"
+                placeholder='Look for more Heroes'
+                onChange={(event) => {
+                    setSearchTerm(event.target.value)
+                }}
             />
-            <ul>
-                {
 
-                    heroes.filter((val)=>{
-                        if(searchTerm==""){
-                            return val
-                        }else if(val.name.toLocaleLowerCase().includes(searchTerm.toLowerCase())){
-                            return val
-                        }
-                    }).map((heroe) => {
-                        return <HeroeListItem key={heroe.id} {...heroe} />
-                    })
-                }
-            </ul>
+            <Grid
+                className="Grid"
+                columnCount={3}
+                columnWidth={200}
+                height={1000}
+                rowCount={1}
+                rowHeight={800}
+                width={1000}
+            >
+                {Cell}
+
+            </Grid>
+
         </div>
+
+
+
+
     )
 }
 
